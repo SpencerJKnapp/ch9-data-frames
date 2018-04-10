01556b6312121e3c332a9b0d35fd4e8d0d3abd36
SpencerJKnapp <knapps@uw.edu>
2018-04-09 19:29:33 -0700

    work on exercise 3 and 4 in class

--- exercise-3/exercise.R
+++ exercise-3/exercise.R
@@ -2,43 +2,56 @@
 
 # Load R's "USPersonalExpenditure" dataset using the `data()` function
 # This will produce a data frame called `USPersonalExpenditure`
-
+data("USPersonalExpenditure")
 
 # The variable `USPersonalExpenditure` is now accessible to you. Unfortunately,
 # it's not a data frame (it's actually what is called a matrix)
 # Test this using the `is.data.frame()` function
-
+is.data.frame(USPersonalExpenditure)
 
 # Luckily, you can pass the USPersonalExpenditure variable as an argument to the
 # `data.frame()` function to convert it a data farm. Do this, storing the
 # result in a new variable
-
+us_personal_expenditure <- data.frame(USPersonalExpenditure)
 
 # What are the column names of your dataframe?
-
+View(us_personal_expenditure)
+colnames(us_personal_expenditure, do.NULL = FALSE)
 
 ## Consider: why are they so strange? Think about whether you could use a number 
 ## like 1940 with dollar notation!
 
-# What are the row names of your dataframe?
+us_personal_expenditure$X1940
 
+# What are the row names of your dataframe?
+row_names <- rownames(us_personal_expenditure, do.NULL = FALSE)
 
 # Add a column "category" to your data frame that contains the rownames
-
+us_personal_expenditure$category <- row_names
 
 # How much money was spent on personal care in 1940?
-
+care_1940 <- us_personal_expenditure[4,1]
 
 # How much money was spent on Food and Tobacco in 1960?
-
+food_1960 <- us_personal_expenditure['Food and Tobacco', '1960']
 
 # What was the highest expenditure category in 1960?
-
+colMax(us_personal_expenditure, na.rm=FALSE, dims = 5)
+highest_1960 <- us_personal_expenditure[us_personal_expenditure$X1960 == max(us_personal_expenditure$X1960)]
 
 # Define a function `lowest_category` that takes in a year as a parameter, and
 # returns the lowest spending category of that year
 
+lowest_category <- function(year) {
+  
+}
 
+
+lowest_category <- function(year) {
+col <- paste0('X', year)
+  us_personal_expenditure$category[us_personal_expenditure[, col] == min(us_personal_expenditure[, col])]
+}
 # Using your function, determine the lowest spending category of each year
 # Hint: use the `sapply()` function to apply your function to a vector of years
-
+all_years <- c("1940", "1945", "1950", "1955", "1960")
+sapply(all_years, lowest_category)
--- exercise-4/exercise.R
+++ exercise-4/exercise.R
@@ -4,36 +4,41 @@
 # file into a variable called `grants` using the `read.csv()`
 # Be sure to set your working directory in RStudio, and do NOT treat strings as 
 # factors!
-
+grants <- read.csv('data/gates_money.csv', stringsAsFactors = FALSE)
 
 # Use the View function to look at the loaded data
-
+View(grants)
 
 # Create a variable `organization` that contains the `organization` column of 
 # the dataset
-
+organization <- grants[,'organization']
 
 # Confirm that the "organization" column is a vector using the `is.vector()` 
 # function. 
 # This is a useful debugging tip if you hit errors later!
-
+is.vector(organization)
 
 ## Now you can ask some interesting questions about the dataset
 
 # What was the mean grant value?
-
+grant_mean <- mean(grants[, 'total_amount'])
+ mean(grants$total_amount)
 
 # What was the dollar amount of the largest grant?
-
+grant_max <- max(grants[, 'total_amount'])
+  max(grants$total_amount)
 
 # What was the dollar amount of the smallest grant?
-
+grant_min <- min(grants[, 'total_amount'])
+  min(grants$total_amount)
 
 # Which organization received the largest grant?
-
+grant_max_org <- grants[grants$total_amount == grant_max, 'organization']
+  organization[grants$total_amount == grant_max]
 
 # Which organization received the smallest grant?
-
-
+grant_min_org <- grants[grants$total_amount == grant_min, 'organization']
+  organization[grants$total_amount == grant_max]
 # How many grants were awarded in 2010?
+grants_2010 <- nrow(grants[grants$start_year == 2010,])
 
